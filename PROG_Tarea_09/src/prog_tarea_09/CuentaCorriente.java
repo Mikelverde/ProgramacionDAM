4c7189e6e3bba844e7765d866f7ec71d8e873f58
Mikelverde <mikelcanfranc@gmail.com>
2019-05-01 15:45:40 +0200

    PROG_Tarea_09 clase CuentaBancaria

--- PROG_Tarea_09/src/prog_tarea_09/CuentaCorriente.java
+++ PROG_Tarea_09/src/prog_tarea_09/CuentaCorriente.java
@@ -5,10 +5,26 @@
  */
 package prog_tarea_09;
 
+import java.util.Hashtable;
+
 /**
  *
  * @author Alf
  */
-public class CuentaCorriente {
+public class CuentaCorriente extends CuentaBancaria {
+    
+    private String cod_entidades;//codigo que identifica a las entidades que pueden
+    //pasar recibos
+    private double recibo_max;//importe maximo de los recibos
+    private Hashtable entidades;
+    
+    public Hashtable set_entidades(String cod_entidades,double recibo_max){
+        Hashtable entidades=new Hashtable<cod_entidades, double>();
+        entidades.put(cod_entidades, recibo_max);
+        return entidades;
+    }
+    
+    
+    
     
 }
