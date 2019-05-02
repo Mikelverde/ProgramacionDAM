64c3ddc976830b0c9359638ccd81bbb86ced3787
Mikelverde <mikelcanfranc@gmail.com>
2019-05-03 01:16:11 +0200

    Bucles swiych funciona solo numerosy try catch en las peticiones de datos

--- PROG_Tarea_09/src/prog_tarea_09/PROG_Tarea_09.java
+++ PROG_Tarea_09/src/prog_tarea_09/PROG_Tarea_09.java
@@ -9,6 +9,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -24,78 +26,130 @@ public class PROG_Tarea_09 {
         Persona mikel = new Persona("mikel", "Fernandez San Higinio", "30/12/81");
         HashSet<CuentaBancaria> lista = new HashSet<>();//En esta lista guardaremos
         //todas las cuentas 
-        HashMap<String,Integer> lista_entidades=new HashMap<>();//En esta lista
-        //guardaremos las dupla de daos entidad y recibo max.
+        HashMap<String, Integer> lista_entidades = new HashMap<>();//En esta lista
+        //guardaremos las dupla de datos entidad y recibo max.
         lista_entidades.put("Recibo Luz", 200);
         lista_entidades.put("Recibo Agua", 100);
         lista_entidades.put("Recibo Seguros", 500);
         lista_entidades.put("Recibo gas", 500);
         lista_entidades.put("Recibo coche", 500);
-        
-        
-        int opcion;
-        int opcion02;
-        Scanner entrada = new Scanner(System.in);
-        entrada.useDelimiter("\n");
-        boolean fin = true;
 
+        int opcion = 0;//la opcion de l primer wich
+        int opcion02 = 0;//la opcion del segundo swich
+        Scanner entrada = new Scanner(System.in);
+        entrada.useDelimiter("\n");//Para que no de problemas la clase scanner
+        boolean fin = true;//para indicar el fin del programa
+        boolean correcto;//se usa para repetir los bules donde se solicitan datos
+        //para el bucle switch hasta que se introduzca una opcion correcta
+        
+        String numero = "";//se usa en los metodos Pattern y Matcher
+//este bucle se ejecutara hasta que se pulse el 0 en la aplicacion y 
+//cambien la variable fin de trua a false
         while (fin) {
-            System.out.println("-----------------------------------------------");
-            System.out.println("Escoja una de las siguientes opciones");
-            System.out.println("1. Nueva Cuenta");
-            System.out.println("2. Listar Cuentas existentes");
-            System.out.println("3. Buscar Cuenta");
-            System.out.println("4. Realizar un ingreso");
-            System.out.println("5. Retirar efectivo");
-            System.out.println("6. Mostrar Saldo de una cuenta");
-            System.out.println("0. Salir de la aplicacion");
-            opcion = entrada.nextInt();
+            correcto = false;//se carga la variable que controla el primer bucle swich
+            while (!correcto) {
+                System.out.println("-----------------------------------------------");
+                System.out.println("Escoja una de las siguientes opciones");
+                System.out.println("1. Nueva Cuenta");
+                System.out.println("2. Listar Cuentas existentes");
+                System.out.println("3. Buscar Cuenta");
+                System.out.println("4. Realizar un ingreso");
+                System.out.println("5. Retirar efectivo");
+                System.out.println("6. Mostrar Saldo de una cuenta");
+                System.out.println("0. Salir de la aplicacion");
+                numero = entrada.next();
+                Pattern p = Pattern.compile("^[0123456]{1}$");//se comprueba que el
+                //valor introducido es uno de las opciones del bucle
+                Matcher m = p.matcher(numero);
+                if (m.find()) {
+                    correcto = true;//como el valor es correcto cambiamos a true
+                    //la variable que controla el bucle
+                    opcion = Integer.parseInt(numero);
+                }
+            }
+            correcto = false;//cargamops false para comenzar el siguiente bucle
+
             switch (opcion) {
                 case 1:
-                    System.out.println("¿Que tipo de cuenta quiere abrir?");
-                    System.out.println("Escoja una de las siguientes opciones");
-                    System.out.println("1. Cuenta Bancaria");
-                    System.out.println("2. Cuenta de Ahorro");
-                    System.out.println("3. Cuenta Corriente");
-                    System.out.println("4. Cuenta Corriente Personal");
-                    System.out.println("5. Cuenta Corriente Empresa");
-                    
 
-                    opcion02 = entrada.nextInt();
+                    while (!correcto) {//se repitira el bucle hasta que se escoja una
+                        //opcion correcta
+                        System.out.println("¿Que tipo de cuenta quiere abrir?");
+                        System.out.println("Escoja una de las siguientes opciones");
+                        System.out.println("1. Cuenta Bancaria");
+                        System.out.println("2. Cuenta de Ahorro");
+                        System.out.println("3. Cuenta Corriente");
+                        System.out.println("4. Cuenta Corriente Personal");
+                        System.out.println("5. Cuenta Corriente Empresa");
+
+                        numero = entrada.next();
+                        Pattern p = Pattern.compile("^[12345]{1}$");//se comprueva el valor
+                        Matcher m = p.matcher(numero);
+                        if (m.find()) {
+                            correcto = true;//valor correcto  cambiamos a true 
+                            opcion02 = Integer.parseInt(numero);
+                        }
+                    }
+
                     switch (opcion02) {
                         case 1:
+                            try{
+                                //pide los datos necesarios para el tipo de cuenta
                             Persona titular = pideTitular();
                             CuentaBancaria cuenta = pideCuenta(titular);
-                            lista.add(cuenta);
+                            lista.add(cuenta);//añade la cuenta a la lista de cuentas
+                            }catch(Exception e){
+                                System.out.println("Dato incorrecto");
+                            }
+                            
                             break;
                         case 2:
+                            try{
+                                 //pide los datos necesarios para el tipo de cuenta
                             Persona titular02 = pideTitular();
                             CuentaBancaria cuenta02 = pideCuenta(titular02);
                             System.out.println("¿Cual va a ser el interes anual?");
                             double int_anual = entrada.nextDouble();
                             CuentaAhorro cuenta_a = new CuentaAhorro(titular02,
                                     cuenta02.getSaldo(), cuenta02.getCcc(), int_anual);
-                            lista.add(cuenta_a);
+                            lista.add(cuenta_a);//añade la cuenta a la lista de cuentas
+                            }catch(Exception e){
+                                System.out.println("Dato incorrecto");
+                            }
+                            
                             break;
                         case 3:
+                             try{
+                                 //pide los datos necesarios para el tipo de cuenta
                             Persona titular03 = pideTitular();
                             CuentaBancaria cuenta03 = pideCuenta(titular03);
                             CuentaCorriente cuenta_c = new CuentaCorriente(titular03,
                                     cuenta03.getSaldo(), cuenta03.getCcc(), lista_entidades);
-                            lista.add(cuenta_c);
+                            lista.add(cuenta_c);//añade la cuenta a la lista de cuentas
+                            }catch(Exception e){
+                                System.out.println("Dato incorrecto");
+                            }
                             
-                            
+
                             break;
                         case 4:
+                             try{
+                                  //pide los datos necesarios para el tipo de cuenta
                             Persona titular04 = pideTitular();
                             CuentaBancaria cuenta04 = pideCuenta(titular04);
                             System.out.println("¿Cual va a ser la comision de mantenimiento?");
                             double mantenimiento = entrada.nextDouble();
                             CuentaCorrientePersonal cuenta_cp = new CuentaCorrientePersonal(titular04,
-                                    cuenta04.getSaldo(), cuenta04.getCcc(),lista_entidades, mantenimiento);
-                            lista.add(cuenta_cp);
+                                    cuenta04.getSaldo(), cuenta04.getCcc(), lista_entidades, mantenimiento);
+                            lista.add(cuenta_cp);//añade la cuenta a la lista de cuentas
+                            }catch(Exception e){
+                                System.out.println("Dato incorrecto");
+                            }
+                           
                             break;
                         case 5:
+                             try{
+                                 //pide los datos necesarios para el tipo de cuenta
                             Persona titular05 = pideTitular();
                             CuentaBancaria cuenta05 = pideCuenta(titular05);
                             System.out.println("¿Cual va a ser el interes por descubierto?");
@@ -103,8 +157,12 @@ public class PROG_Tarea_09 {
                             System.out.println("¿Cual va a ser el maximo descubierto permitido?");
                             double max_descubierto = entrada.nextDouble();
                             CuentaCorrienteEmpresa cuenta_ce = new CuentaCorrienteEmpresa(titular05,
-                                    cuenta05.getSaldo(), cuenta05.getCcc(),lista_entidades, int_descubierto,max_descubierto);
-                            lista.add(cuenta_ce);
+                                    cuenta05.getSaldo(), cuenta05.getCcc(), lista_entidades, int_descubierto, max_descubierto);
+                            lista.add(cuenta_ce);//añade la cuenta a la lista de cuentas
+                            }catch(Exception e){
+                                System.out.println("Dato incorrecto");
+                            }
+                            
                             break;
 
                     }
@@ -123,21 +181,21 @@ public class PROG_Tarea_09 {
                 case 4:
                     System.out.println("Importe del ingreso");
                     double ingreso = entrada.nextDouble();
-                    CuentaBancaria cuenta_ingreso=buscaCuenta(lista);
+                    CuentaBancaria cuenta_ingreso = buscaCuenta(lista);
                     cuenta_ingreso.ingresar(ingreso);
-                    System.out.println("El nuevo saldo es de "+cuenta_ingreso.getSaldo());
+                    System.out.println("El nuevo saldo es de " + cuenta_ingreso.getSaldo());
 
                     break;
-                    
+
                 case 5:
                     System.out.println("Importe de la retirada");
                     double retirada = entrada.nextDouble();
-                    CuentaBancaria cuenta_retirada=buscaCuenta(lista);
+                    CuentaBancaria cuenta_retirada = buscaCuenta(lista);
                     cuenta_retirada.retirar(retirada);
-                    System.out.println("El nuevo saldo es de "+cuenta_retirada.getSaldo());
+                    System.out.println("El nuevo saldo es de " + cuenta_retirada.getSaldo());
                     break;
                 case 6:
-                    System.out.println("El saldo de su cuenta es de "+buscaCuenta(lista).getSaldo()+"€");
+                    System.out.println("El saldo de su cuenta es de " + buscaCuenta(lista).getSaldo() + "€");
                     break;
                 case 0:
                     fin = false;
